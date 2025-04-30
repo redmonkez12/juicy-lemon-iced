ec98040c4565842a1ca7e1d34dc33db6e9a99cd4
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-04-30 22:16:14 +0200

    Display instruments

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ mod symbols;
 use iced::Task;
 
 use crate::symbols::get_symbols;
-use iced::widget::container;
+use iced::widget::{Column, text, Row, Scrollable};
 use iced::{Element, Theme};
+use tokio::runtime::Runtime;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Message {
     FetchSymbols,
     SymbolsFetched(Result<Vec<String>, String>),
@@ -14,6 +15,7 @@ enum Message {
 #[derive(Default)]
 struct State {
     instruments: Vec<String>,
+    loading: bool,
 }
 
 fn theme(_: &State) -> Theme {
@@ -21,31 +23,70 @@ fn theme(_: &State) -> Theme {
 }
 
 fn view(state: &State) -> Element<Message> {
-    container("I am 300px tall!").height(300).into()
+    if state.loading {
+        return text("Loading...").size(20).into();
+    }
+
+    if state.instruments.is_empty() {
+        return text("Nothing found").size(20).into();
+    }
+
+    println!("{:?}", &state.instruments);
+
+    let mut instrument_list = Column::new().spacing(10);
+
+    for instrument in &state.instruments {
+        let row = Row::new().push(text(instrument).size(20));
+        instrument_list = instrument_list.push(row);
+    }
+
+    Scrollable::new(instrument_list).into()
 }
+
 
 fn update(state: &mut State, message: Message) -> Task<Message> {
     match message {
         Message::FetchSymbols => {
+            let rt = Runtime::new().unwrap();
             println!("Fetching symbols");
+            state.loading = true;
             Task::perform(
-                get_symbols(),
+                async move { rt.block_on(get_symbols()) },
                 Message::SymbolsFetched,
             )
-        },
+        }
         Message::SymbolsFetched(Ok(instruments)) => {
+            println!("Symbols fetched: {} instruments", instruments.len());
             state.instruments = instruments;
+            state.loading = false;
             Task::none()
         }
-        _ => {
+        Message::SymbolsFetched(Err(error)) => {
+            println!("Error fetching symbols: {}", error);
+            state.loading = false;
             Task::none()
         }
     }
 }
 
-// #[tokio::main]
+fn init() -> (State, Task<Message>) {
+    let state = State {
+        instruments: Vec::new(),
+        loading: true,
+    };
+    let rt = Runtime::new().unwrap();
+
+    (
+        state,
+        Task::perform(
+            async move { rt.block_on(get_symbols()) },
+            Message::SymbolsFetched,
+        ),
+    )
+}
+
 fn main() -> iced::Result {
     iced::application("Juicy Lemon", update, view)
         .theme(theme)
-        .run()
+        .run_with(init)
 }
